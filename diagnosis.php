58491040739f96d7605ee9e86c7366208de76aab
Yuina-Kage <yuina.kg@gmail.com>
2020-04-16 23:06:08 +0900

    メインページ作成

---
+++ diagnosis.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>診断の画面</title>
+</head>
+<body>
+  
+</body>
+</html>
--- index.php
+++ index.php
@@ -8,4 +8,26 @@ $dbh = connectDb();
 ?>
 
 
+<!DOCTYPE html>
+<html lang="ja">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>パーソナルカラー診断アプリ</title>
+</head>
+
+<body>
+  <header class="pc-header">
+    <li><a href="about.php">パーソナルカラーとは</a></li>
+    <li><a href="result.php">女性のタイプ</a></li>
+    <li><a href="result.php">男性のタイプ</a></li>
+    <li><a href="diagnosis.php">診断スタート</a></li>
+    <li><a href="post.php">Twitter</a></li>
+
+  </header>
+  <h1>パーソナルカラー診断</h1>
+</body>
+
+</html>
