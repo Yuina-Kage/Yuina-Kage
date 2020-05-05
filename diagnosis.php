82b2204c91d35604241a9c892c4d657f4f453a40
Yuina-Kage <yuina.kg@gmail.com>
2020-05-05 19:34:04 +0900

    アドバイスお願い致します

--- diagnosis.php
+++ diagnosis.php
@@ -5,6 +5,8 @@ require_once('functions.php');
 
 
 $id = $_GET['id'];
+$yes_id = 'yes_id';
+$no_id = 'no_id';
 
 $dbh = connectDb();
 
@@ -12,42 +14,40 @@ $sql = 'SELECT * FROM diagnoses WHERE id = :id';
 $stmt = $dbh->prepare($sql);
 $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
-
 $diagnoses = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-  $yes_id = 'yes_id';
-  $no_id = 'no_id';
+$sql = 'SELECT * FROM diagnoses WHERE yes_id';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
 
 
-  $sql = 'SELECT * FROM diagnoses WHERE yes_id = :yes_id';
-  $stmt = $dbh->prepare($sql);
-  $stmt->bindParam(':yes_id', $yes_id, PDO::PARAM_INT);
-  $stmt->execute();
-  $diagnoses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$sql = 'SELECT * FROM diagnoses WHERE no_id';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
 
 
-  $sql = 'SELECT * FROM diagnoses WHERE no_id = :no_id';
-  $stmt = $dbh->prepare($sql);
-  $stmt->bindParam(':no_id', $no_id, PDO::PARAM_INT);
-  $stmt->execute();
-  $diagnoses = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+if ($yes_id = 2) {
+  $sql = 'SELECT * FROM diagnoses WHERE id=2';
+if ($no_id = 13) {
+  $sql = 'SELECT * FROM diagnoses WHERE id=13';
 }
-
-if($yes_id=2) {
-
-}
-
-if($no_id=13){
-  
 }
 
 
 
+$sql = 'SELECT * FROM diagnoses WHERE id = 2';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+if ($yes_id = 3){
+  $sql = 'SELECT * FROM diagnoses WHERE id=3';
+if ($no_id = 4){
+  $sql = 'SELECT * FROM diagnoses WHERE id=4';
+}
+}
 
 
 
@@ -57,6 +57,15 @@ if($no_id=13){
 
 
 
+
+
+
+
+
+
+// パラメータで渡されたidを元にデータを取得する
+// はい、いいえボタンに取得してきたデータのyes_idとno_idを、idというパラメータ名でaタグに設定する
+// はい、いいえのボタンを押されたら、1. 2.の処理を繰り返す
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -72,14 +81,13 @@ if($no_id=13){
   <ul>
     <?php foreach ($diagnoses as $diagnosis) : ?>
       <li>
-
         <?php echo h($diagnosis['content']); ?>
       </li>
     <?php endforeach; ?>
-
-
-    <a href="diagnosis.php&id=<?php echo $yes_id; ?>">はい</a>
-    <a href="diagnosis.php&id=<?php echo $no_id; ?>">いいえ</a>
+  
+    
+    <a href="diagnosis.php?id=<?php echo $yes_id; ?>">はい</a>
+    <a href="diagnosis.php?id=<?php echo $no_id; ?>">いいえ</a>
 </body>
 
 </html>
