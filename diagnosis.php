07354784d024e7476f92250cc3c9759f77c1bdc8
Yuina-Kage <yuina.kg@gmail.com>
2020-05-16 23:55:35 +0900

    診断画面の表示完了

--- diagnosis.php
+++ diagnosis.php
@@ -5,8 +5,10 @@ require_once('functions.php');
 
 
 $id = $_GET['id'];
-$yes_id = 'yes_id';
-$no_id = 'no_id';
+$yes_id = $_GET['yes_id'];
+$no_id = $_GET['no_id'];
+
+
 
 $dbh = connectDb();
 
@@ -14,58 +16,10 @@ $sql = 'SELECT * FROM diagnoses WHERE id = :id';
 $stmt = $dbh->prepare($sql);
 $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
-$diagnoses = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-$sql = 'SELECT * FROM diagnoses WHERE yes_id';
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-
-
-$sql = 'SELECT * FROM diagnoses WHERE no_id';
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
+$diagnosis = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
 
-
-if ($yes_id = 2) {
-  $sql = 'SELECT * FROM diagnoses WHERE id=2';
-if ($no_id = 13) {
-  $sql = 'SELECT * FROM diagnoses WHERE id=13';
-}
-}
-
-
-
-$sql = 'SELECT * FROM diagnoses WHERE id = 2';
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-
-if ($yes_id = 3){
-  $sql = 'SELECT * FROM diagnoses WHERE id=3';
-if ($no_id = 4){
-  $sql = 'SELECT * FROM diagnoses WHERE id=4';
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// パラメータで渡されたidを元にデータを取得する
-// はい、いいえボタンに取得してきたデータのyes_idとno_idを、idというパラメータ名でaタグに設定する
-// はい、いいえのボタンを押されたら、1. 2.の処理を繰り返す
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -78,16 +32,14 @@ if ($no_id = 4){
 </head>
 
 <body>
-  <ul>
-    <?php foreach ($diagnoses as $diagnosis) : ?>
-      <li>
-        <?php echo h($diagnosis['content']); ?>
-      </li>
-    <?php endforeach; ?>
+  <h2><?php echo h($diagnosis['content']); ?></h2>
+  <p>
+    <a href="diagnosis.php?id=<?php echo h($diagnosis['yes_id']); ?>">はい</a>
+    <a href="diagnosis.php?id=<?php echo h($diagnosis['no_id']); ?>">いいえ</a>
+  </p>
   
-    
-    <a href="diagnosis.php?id=<?php echo $yes_id; ?>">はい</a>
-    <a href="diagnosis.php?id=<?php echo $no_id; ?>">いいえ</a>
+    <a href="result.php?id=<?php echo h($diagnosis['yes_type_id']); ?>"></a>
+    <a href="result.php?id=<?php echo h($diagnosis['no_type_id']); ?>"></a>
 </body>
 
 </html>
--- result.php
+++ result.php
@@ -5,14 +5,25 @@ require_once('functions.php');
 
 $dbh = connectDb();
 
-$sql = "SELECT * FROM types where id=1";
+$id = $_GET['id'];
+$yes_type_id = $_GET['yes_type_id'];
+$no_type_id = $_GET['no_type_id'];
+
+
+$sql = 'SELECT * FROM types WHERE id = :id';
 $stmt = $dbh->prepare($sql);
+$stmt->bindParam(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
-$types = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$type = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
-
-
+if($yes_type_id = 1){
+  $sql = 'SELECT * FROM types WHERE id = 1';
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+  $stmt->execute();
+  $type = $stmt->fetch(PDO::FETCH_ASSOC);
+}
 ?>
 
 <!DOCTYPE html>
@@ -38,9 +49,14 @@ $types = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <?php echo h($type['hair_color']); ?><br>
       </li>
     <?php endforeach; ?>
-    
+
+    <a href="result.php?id=<?php echo h($diagnosis['id']); ?>"></a>
+    <a href="result.php?id=<?php echo h($diagnosis['id']); ?>"></a>
+
     <img src="images/woman_type.jpg" alt="女性のタイプの画像">
     <img src="images/man_type.jpg" alt="男性のタイプの画像">
+
+
 </body>
 
 </html>
